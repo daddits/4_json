2103ecc2b2268217845b0b49058fe4cf7702bdfc
Леонид Гилькович <4aisl.leo@gmail.com>
2017-10-25 17:47:26 +0300

    replace value

--- pprint_json.py
+++ pprint_json.py
@@ -7,64 +7,64 @@ def load_data(file_path):
 
 
 def pretty_print_json(json_content):
-    for value in json_content:
+    for shop in json_content:
         for x in range(3):
             print(json.dumps([
                 {
                     'Cells': {
-                        'Address': value[x]['Cells']['Address'],
-                        'AdmArea': value[x]['Cells']['AdmArea'],
-                        'ClarificationOfWorkingHours': value[x]['Cells']['ClarificationOfWorkingHours'],
-                        'District': value[x]['Cells']['District'],
-                        'IsNetObject': value[x]['Cells']['IsNetObject'],
-                        'Name': value[x]['Cells']['Name'],
-                        'OperatingCompany': value[x]['Cells']['OperatingCompany'],
+                        'Address': shop[x]['Cells']['Address'],
+                        'AdmArea': shop[x]['Cells']['AdmArea'],
+                        'ClarificationOfWorkingHours': shop[x]['Cells']['ClarificationOfWorkingHours'],
+                        'District': shop[x]['Cells']['District'],
+                        'IsNetObject': shop[x]['Cells']['IsNetObject'],
+                        'Name': shop[x]['Cells']['Name'],
+                        'OperatingCompany': shop[x]['Cells']['OperatingCompany'],
                         'PublicPhone': [
                             {
-                                'PublicPhone': value[x]['Cells']['PublicPhone'][0]['PublicPhone']
+                                'PublicPhone': shop[x]['Cells']['PublicPhone'][0]['PublicPhone']
                             }
                         ],
-                        'TypeService': value[x]['Cells']['TypeService'],
+                        'TypeService': shop[x]['Cells']['TypeService'],
                         'WorkingHours': [{
-                            'DayOfWeek': value[x]['Cells']['WorkingHours'][0]['DayOfWeek'],
-                            'Hours': value[x]['Cells']['WorkingHours'][0]['Hours']
+                            'DayOfWeek': shop[x]['Cells']['WorkingHours'][0]['DayOfWeek'],
+                            'Hours': shop[x]['Cells']['WorkingHours'][0]['Hours']
                          },
                          {
-                            'DayOfWeek': value[x]['Cells']['WorkingHours'][1]['DayOfWeek'],
-                            'Hours': value[x]['Cells']['WorkingHours'][1]['Hours']
+                            'DayOfWeek': shop[x]['Cells']['WorkingHours'][1]['DayOfWeek'],
+                            'Hours': shop[x]['Cells']['WorkingHours'][1]['Hours']
                          },
                          {
-                            'DayOfWeek': value[x]['Cells']['WorkingHours'][2]['DayOfWeek'],
-                            'Hours': value[x]['Cells']['WorkingHours'][2]['Hours']
+                            'DayOfWeek': shop[x]['Cells']['WorkingHours'][2]['DayOfWeek'],
+                            'Hours': shop[x]['Cells']['WorkingHours'][2]['Hours']
                          },
                          {
-                            'DayOfWeek': value[x]['Cells']['WorkingHours'][3]['DayOfWeek'],
-                            'Hours': value[x]['Cells']['WorkingHours'][3]['Hours']
+                            'DayOfWeek': shop[x]['Cells']['WorkingHours'][3]['DayOfWeek'],
+                            'Hours': shop[x]['Cells']['WorkingHours'][3]['Hours']
                          },
                          {
-                            'DayOfWeek': value[x]['Cells']['WorkingHours'][4]['DayOfWeek'],
-                            'Hours': value[x]['Cells']['WorkingHours'][4]['Hours']
+                            'DayOfWeek': shop[x]['Cells']['WorkingHours'][4]['DayOfWeek'],
+                            'Hours': shop[x]['Cells']['WorkingHours'][4]['Hours']
                          },
                          {
-                            'DayOfWeek': value[x]['Cells']['WorkingHours'][5]['DayOfWeek'],
-                            'Hours': value[x]['Cells']['WorkingHours'][5]['Hours']
+                            'DayOfWeek': shop[x]['Cells']['WorkingHours'][5]['DayOfWeek'],
+                            'Hours': shop[x]['Cells']['WorkingHours'][5]['Hours']
                          },
                          {
-                            'DayOfWeek': value[x]['Cells']['WorkingHours'][6]['DayOfWeek'],
-                            'Hours': value[x]['Cells']['WorkingHours'][6]['Hours']
+                            'DayOfWeek': shop[x]['Cells']['WorkingHours'][6]['DayOfWeek'],
+                            'Hours': shop[x]['Cells']['WorkingHours'][6]['Hours']
                          }
                         ],
                         'geoData': {
                            'coordinates': [
-                               value[x]['Cells']['geoData']['coordinates'][0],
-                               value[x]['Cells']['geoData']['coordinates'][1]
+                               shop[x]['Cells']['geoData']['coordinates'][0],
+                               shop[x]['Cells']['geoData']['coordinates'][1]
                             ],
-                           'type': value[x]['Cells']['geoData']['type']
+                           'type': shop[x]['Cells']['geoData']['type']
                         },
-                        'global_id': value[x]['Cells']['global_id']
+                        'global_id': shop[x]['Cells']['global_id']
                     },
-                    'Id': value[x]['Id'],
-                    'Number': value[x]['Number']
+                    'Id': shop[x]['Id'],
+                    'Number': shop[x]['Number']
                  }], sort_keys=True, indent=4))
 
 
